47caf7935b7a678b9cceb33f2bcfc0b55adb9da2
Yoselyn Miranda <91152700+Yoselyn214@users.noreply.github.com>
2024-05-15 19:45:46 -0500

    Add files via upload

---
+++ VerificarUsuario.py
@@ -0,0 +1,36 @@
+def lambda_handler(event, context):
+    # Obtener los parámetros de la solicitud GET
+    username = event['Username']
+    password = event['Contrasena']
+
+    # Conectar a DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('User')  # Nombre de tu tabla de usuarios
+
+    # Consultar la base de datos para buscar al usuario por nombre de usuario
+    response = table.query(
+        IndexName='Username-index', 
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('Username').eq(username)
+    )
+    # Verificar si el usuario existe en la base de datos
+    if response['Items']:
+        # El usuario existe, ahora verifica la contraseña
+        user_data = response['Items'][0]
+        if user_data['Contrasena'] == password:
+            # La contraseña coincide, usuario autenticado
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'message': 'Usuario autenticado'})
+            }
+        else:
+            # La contraseña no coincide
+            return {
+                'statusCode': 401,
+                'body': json.dumps({'message': 'Contraseña incorrecta'})
+            }
+    else:
+        # El usuario no existe
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'message': 'Usuario no encontrado'})
+        }
