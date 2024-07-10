231dac041ec9cb32598ba2a2f332b5450c617a3f
Yoselyn Miranda <91152700+Yoselyn214@users.noreply.github.com>
2024-07-10 00:26:41 -0500

    Update getallPosts.py

--- getallPosts.py
+++ getallPosts.py
@@ -1,23 +1,42 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+from decimal import Decimal
 
 # Obtener todos los registros de la tabla Post
 def lambda_handler(event, context):
     # Conectar a DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    comment_table = dynamodb.Table('Post')
+    post_table = dynamodb.Table('Post')
 
     try:
-        response = comment_table.scan()
+        response = post_table.scan()
         posts = response.get('Items', [])
     except ClientError as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'message': 'Error al obtener las publicaciones', 'error': str(e)})
-        }
+        print(f"ClientError: {e}")
+        return generate_response(500, {'message': 'Error al obtener las publicaciones', 'error': str(e)})
+    except Exception as e:
+        print(f"Exception: {e}")
+        return generate_response(500, {'message': 'Error interno del servidor', 'error': str(e)})
 
+    return generate_response(200, posts)
+
+def generate_response(status_code, body):
     return {
-        'statusCode': 200,
-        'body': posts
+        'statusCode': status_code,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        'body': json.dumps(body, default=decimal_default)
     }
+
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        # Convertir a int si no tiene parte fraccionaria, de lo contrario convertir a float
+        if obj % 1 == 0:
+            return int(obj)
+        else:
+            return float(obj)
+    raise TypeError(f'Object of type {obj.__class__.__name__} is not JSON serializable')
