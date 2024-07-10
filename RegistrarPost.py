7bd6ecc1f9498474640a46a02be2cf064701d1a9
Yoselyn Miranda <91152700+Yoselyn214@users.noreply.github.com>
2024-07-10 00:53:45 -0500

    Update RegistrarPost.py

--- RegistrarPost.py
+++ RegistrarPost.py
@@ -6,7 +6,7 @@ from botocore.exceptions import ClientError
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    # Asume que el cuerpo de la solicitud contiene un JSON con 'Contenido', 'Usuario_id', 'Likes', 'Fecha', 'Hora' y 'Imagen' (en base64)
+    # Asume que el cuerpo de la solicitud contiene un JSON con 'Contenido', 'Usuario_id', 'Username', 'Likes', 'Fecha', 'Hora' y 'Imagen' (en base64)
     body = event
     bucket_name = 'imagenessoftware'
     image_data = base64.b64decode(body['Imagen'])
@@ -15,6 +15,30 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     post_table = dynamodb.Table('Post')
     counter_pos = dynamodb.Table('CounterPosts')
+    user_table = dynamodb.Table('User')
+
+    # Verificar si el usuario existe
+    try:
+        # Imprimir el key que se está utilizando para la verificación
+        print(f"Verificando existencia del Usuario_id: {body['Usuario_id']} y Username: {body['Username']}")
+        
+        user_response = user_table.get_item(Key={'Usuario_id': int(body['Usuario_id']), 'Username': body['Username']})
+        if 'Item' not in user_response:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Usuario no existe'})
+            }
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Error al verificar el usuario', 'error': str(e)})
+        }
+    except Exception as e:
+        # Captura cualquier otro error y proporciona más información de depuración
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Error desconocido al verificar el usuario', 'error': str(e)})
+        }
 
     try:
         # Incrementar el contador y obtener el nuevo Post_id
@@ -44,6 +68,7 @@ def lambda_handler(event, context):
     Post = {
         'Post_id': int(Post_id),
         'Usuario_id': int(body['Usuario_id']),
+        'Username': body['Username'],
         'Contenido': body['Contenido'],
         'Imagen': image_url,
         'Likes': int(body['Likes']),
