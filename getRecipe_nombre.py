832e595e95c4cce93315d0578cef0e50890fe651
Sebastian Tenorio <103154944+Yellow2392@users.noreply.github.com>
2024-07-10 14:32:07 -0500

    Update getRecipe_nombre.py

--- getRecipe_nombre.py
+++ getRecipe_nombre.py
@@ -1,38 +1,29 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
-from boto3.dynamodb.conditions import Key
 
+# Obtener todos los registros de la tabla Post
 def lambda_handler(event, context):
-    # Obtener el nombre del cuerpo de la solicitud
-    body = event
-    nombre = body['Nombre']
-    
-    if not nombre:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'message': 'Nombre no proporcionado'})
-        }
-
     # Conectar a DynamoDB
     dynamodb = boto3.resource('dynamodb')
     recipe_table = dynamodb.Table('Recetas')
 
     try:
-        # Usar query con el índice secundario global
-        response = recipe_table.query(
-            IndexName='Nombre-index',
-            KeyConditionExpression=Key('Nombre').eq(nombre),
-            Limit=100
-        )
+        response = recipe_table.scan(Limit=100)
         recipes = response.get('Items', [])
     except ClientError as e:
         return {
             'statusCode': 500,
             'body': json.dumps({'message': 'Error al obtener las publicaciones', 'error': str(e)})
         }
+    
+    result = []
+    for elem in recipes:
+        registro = elem.get("Nombre")
+        if (event["Nombre"]).lower() in registro.lower():
+            result.append(elem)
 
     return {
         'statusCode': 200,
-        'body': recipes
+        'body': result
     }
