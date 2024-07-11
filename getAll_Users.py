a2692cf12fcfccaf22c2e43f5ff033ca31ce10eb
Sebastian Tenorio <103154944+Yellow2392@users.noreply.github.com>
2024-07-10 19:48:33 -0500

    Update getAll_Users.py

--- getAll_Users.py
+++ getAll_Users.py
@@ -1,23 +1,43 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+from decimal import Decimal
+
+# Función personalizada para convertir Decimal a float/int
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        # Convertir a int si no tiene parte fraccionaria, de lo contrario convertir a float
+        if obj % 1 == 0:
+            return int(obj)
+        else:
+            return float(obj)
+    raise TypeError
 
 # Obtener todos los registros de la tabla User
 def lambda_handler(event, context):
     # Conectar a DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    recipe_table = dynamodb.Table('User')
+    user_table = dynamodb.Table('User')
 
     try:
-        response = recipe_table.scan()
-        recipes = response.get('Items', [])
+        response = user_table.scan()
+        users = response.get('Items', [])
     except ClientError as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({'message': 'Error al obtener los usuarios', 'error': str(e)})
+            'body': json.dumps({'message': 'Error al obtener los usuarios', 'error': str(e)}),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }
         }
 
     return {
         'statusCode': 200,
-        'body': recipes
+        'body': json.dumps({'Items': users}, default=decimal_default),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            
+        },
     }
